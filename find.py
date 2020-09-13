765e845b5913271217955dc1a739522186a1ec07
divyashk <dk0018w@gmail.com>
2020-09-13 14:18:35 +0530

    added the emailing setup

--- find.py
+++ find.py
@@ -1,6 +1,12 @@
 import os
 import re
 import datetime
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
 
 # storing the present time and date
 datetime_now = datetime.datetime.now()
@@ -16,12 +22,12 @@ date_comp = day+"/"+month+"/"+year
   # first for openvpn
 os.system('service openvpn status > services-status')
 
-with open('services-status','a') as f_write:              # just to seperate the them :)
+with open('services-status','a') as f_write:              # just to seperate them :)
     f_write.write("\n\n\n\n\n")
 
 os.system('service network-manager status >> services-status')
 
-# You can add more services here that feel like monitoring
+# You can add more services here that you feel like monitoring
 #
 # with open('services-status','a') as f_write:             
 #     f_write.write("\n\n\n")
@@ -34,7 +40,7 @@ os.system('service network-manager status >> services-status')
 
 
 
-
+# NOTE - this part will run only if you are using Nginx instead of Apache
 
 # # creating nginx-access file report 
 # with open('/var/log/nginx/access.log', 'r', encoding='utf-8') as read_file:
@@ -45,7 +51,6 @@ os.system('service network-manager status >> services-status')
       
 #       for line in lines:
 #           pattern_dist = re.compile(r'(?P<ip>^\w*.\w*.\w*.\w*\s)-\s-\s\[(?P<date>[^\s^:]*):(?P<time>[^\s]*\s)[^\s]* \"(?P<req>[^\"]*)\" (?P<status>\w*\s)(?P<details>.*\s)')
-                           
 #           obj_grps = pattern_dist.search(line);
       
 #           if obj_grps.group('date') == date_comp:
@@ -53,9 +58,6 @@ os.system('service network-manager status >> services-status')
 #             f_write.write('{0:5}\t{1:28}\t{2:20}\t{3:10}\t{4:20}\t{5:50}\n\n'.format(obj_grps.group("status"),obj_grps.group("req"),obj_grps.group("ip"),obj_grps.group("time"),obj_grps.group("date"),obj_grps.group("details")))
     
 #     f_write.close()
-
-
-
 
 
 
@@ -77,9 +79,6 @@ with open('/var/log/syslog', 'r', encoding='utf-8') as read_file:
               f_write.write('{0:2}\t{1:4}\t{2:10}\t{3:15}\t{4:25}\t{5:50}\n\n'.format(obj_grps.group("day"),obj_grps.group("month"),obj_grps.group("time"),obj_grps.group("logger"),obj_grps.group("application"),obj_grps.group("message")))
       f_write.close()
       
-
-
-
 
 # reading from /var/log/auth.log
 with open('/home/dk/Desktop/auth.log', 'r', encoding='utf-8') as read_file:
@@ -119,4 +118,92 @@ with open('/var/log/faillog', 'r', encoding='utf-8') as read_file:
               f_write.write('{0:2}\t{1:4}\t{2:10}\t{3:15}\t{4:25}\t{5:50}\n\n'.format(obj_grps.group("day"),obj_grps.group("month"),obj_grps.group("time"),obj_grps.group("logger"),obj_grps.group("application"),obj_grps.group("message")))
       f_write.close()
       
-      
+  
+# Sending the email
+
+
+email_send = os.environ.get('SEND_EMAIL')                                 
+email_user = os.environ.get('MY_EMAIL')
+email_password = os.environ.get('MY_PASSWORD')
+
+subject = 'Daily server report'
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_send
+msg['Subject'] = subject
+
+body = 'Please find the attached copies of your server reports'
+
+msg.attach(MIMEText(body,'plain'))
+
+filename='authlog-report'
+attachment  =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+
+
+filename='authlog-report'
+attachment  =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+
+filename='faillog-report'
+attachment  =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+
+filename='syslog-report'
+attachment  =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+
+# Only for nginx 
+filename='nginx-access-report'
+attachment  =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+
+filename='services-status'
+attachment  =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+
+text = msg.as_string()
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(email_user,email_password)
+
+
+server.sendmail(email_user,email_send,text)
+server.quit()
